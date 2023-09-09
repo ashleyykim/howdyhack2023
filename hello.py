1dbac79202013f005332a7656ae8487d22b2a9b4
Ashley Kim <ashleykim@client-10-229-0-74.tamulink.tamu.edu>
2023-09-09 16:31:19 -0500

    spotipy

--- hello.py
+++ hello.py
@@ -1,14 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+cid = 'c4ef5ca0a62a464bae63c4dc44cef3d3'
+secret = 'Your Secret ID'
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager
+=
+client_credentials_manager)
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/process', methods=['GET'])
+def process():
+    data = request.get_json()
+    name = data['name']
+    email = data['email']
 
-@app.route("/wow")
-def wow():
-    return "<p>Wow!</p>"
+    # get data from spotify
+@app.route("/getSpotify")
+def getSpotify():
+    
+
+    # Return response back to frontend if connection successful
+    response = {
+        "status": "success",
+        "message": "Data received successfully!"
+    }
+
+    return jsonify(response)
+    
 
 
 if __name__ == "__main__":
