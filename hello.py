23dbe596a14402e2dd8a00d05d962dbb1c6a763b
Ashley Kim <ashleykim@client-10-229-0-74.tamulink.tamu.edu>
2023-09-09 17:30:45 -0500

    songs

--- hello.py
+++ hello.py
@@ -1,57 +1,34 @@
-from flask import Flask, request, jsonify
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+import requests
+import os
 
 cid = 'c4ef5ca0a62a464bae63c4dc44cef3d3'
 secret = '86b0c8f6cccf46ccb4eb9eac3e53f369'
 redirect_uri = 'http://localhost:3000'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cid,
-                                               client_secret=secret,
-                                               redirect_uri=redirect_uri,
-                                               scope='user-library-read user-modify-playback-state user-read-playback-state'))
 
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-# sp = spotipy.Spotify(client_credentials_manager
-# =
-# client_credentials_manager)
+playlist_uri = 'spotify:playlist:37i9dQZF1DX0011TOiJEnw'
 
+results = sp.playlist_tracks(playlist_uri)
+tracks = results['items']
 
-# Replace 'YOUR_PLAYLIST_ID' with the actual ID of the playlist
-playlist_id = '37i9dQZF1DX0011TOiJEnw'
-results = sp.playlist_tracks(playlist_id)
+if not os.path.exists('playlist_audio'):
+    os.makedirs('playlist_audio')
 
-# Extract the first track from the playlist
-first_track = results['items'][0]['track']
+for track in tracks:
+    track_name = track['track']['name']
+    track_preview_url = track['track']['preview_url']
 
-if first_track['preview_url']:
-    preview_url = first_track['preview_url']
-    sp.start_playback(uris=[first_track['uri']])
-
-app = Flask(__name__)
-
-@app.route('/process', methods=['GET'])
-def process():
-    data = request.get_json()
-    name = data['name']
-    email = data['email']
-
-    # get data from spotify
-# @app.route("/getSpotify")
-# def getSpotify():
-#     GET/playlists{37i9dQZF1DX0011TOiJEnw}
-
-    
-
-    # Return response back to frontend if connection successful
-    response = {
-        "status": "success",
-        "message": "Data received successfully!"
-    }
-
-    return jsonify(response)
-    
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)  
+    if track_preview_url:
+        # Download the audio file
+        response = requests.get(track_preview_url)
+        if response.status_code == 200:
+            with open(f'playlist_audio/{track_name}.mp3', 'wb') as f:
+                f.write(response.content)
+        else:
+            print(f"Failed to download {track_name}")
+    else:
+        print(f"No preview available for {track_name}")
