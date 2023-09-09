136cf32b9b5eb13c8fe975ca4d5d940ce8bd757a
Ashley Kim <ashleykim@client-10-229-0-74.tamulink.tamu.edu>
2023-09-09 18:48:23 -0500

    more songs

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const musicFiles = [
     { title: 'Blame It on the Boogie', url: 'playlist_audio/Blame It on the Boogie.mp3' },
     { title: 'Dancing Queen', url: 'playlist_audio/Dancing Queen.mp3' },
     { title: 'Heaven Is A Place On Earth', url: 'playlist_audio/Heaven Is A Place On Earth - Promo 7 Edit.mp3' }
+    { title: 'I Will Survive - Remastered', url: 'playlist_audio/I Will Survive - Remastered.mp3' },
+    { title: 'Just Can\'t Get Enough - 2006 Remaster', url: 'playlist_audio/Just Can\'t Get Enough - 2006 Remaster' },
+    { title: 'Karma Chameleon - Remastered 2002', url: 'playlist_audio/Karma Chameleon - Remastered 2002.mp3' }
+    { title: 'Like A Prayer', url: 'playlist_audio/Like A Prayer.mp3' },
+    { title: 'My Sharona', url: 'playlist_audio/My Sharona.mp3' },
+    { title: 'Never Gonna Give You Up', url: 'playlist_audio/Never Gonna Give You Up.mp3' }
 ];
 let currentSongIndex = 0;
 
